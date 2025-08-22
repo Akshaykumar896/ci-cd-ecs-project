2d02d30fb3114406d23b9d869295812b44926fae
Akshay <prajapatiakshay8968@gmail.com>
2025-08-22 23:15:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from ECS Fargate!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
